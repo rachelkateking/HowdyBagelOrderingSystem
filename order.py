97967b781a0b5967db2a59c8d4bd9e72976064b4
rachelkateking <rachel.kate.king@gmail.com>
2025-08-19 17:59:09 -0700

    Add hh discount

--- order.py
+++ order.py
@@ -1,14 +1,20 @@
 from typing import List
 from menu import MENU, TOPPINGS
 
+HAPPY_HOUR_DISCOUNT = 0.2
+
 def calculate_bagel_cost(items: List[str]) -> float:
     return sum(MENU[item] for item in items if item in MENU)
 
 def calculate_topping_cost(items: List[str]) -> float:
     return sum(TOPPINGS[item] for item in items if item in TOPPINGS)
 
+def calculate_hh_discount(subtotal: float) -> float:
+    return subtotal * (1-HAPPY_HOUR_DISCOUNT)
+
 def calculate_total(bagels: List[str], toppings: List[str]) -> float:
     subtotal: float = calculate_bagel_cost(bagels) + calculate_topping_cost(toppings)
+    subtotal = calculate_hh_discount(subtotal)
     return round(subtotal, 2)
 
 def print_receipt(bagels: List[str], toppings: List[str], total: float) -> None:
